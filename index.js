31be5c6a99a8c8c7c5aad5573e2db5b611ceeaa9
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-10 19:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const currentUser = 'Noah';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGretting = `Welcome, ${currentUser}(1)!`
